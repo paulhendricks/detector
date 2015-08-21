49005fde7a3cbe472c6505ca3aa31536b757bf3b
paulhendricks <paul.hendricks.2013@owu.edu>
2015-08-21 16:40:39 +0000

    First commit.

---
+++ R/detector.R
@@ -0,0 +1,8 @@
+#' detector: Detect Data Containing Personally Identifiable Information
+#'
+#' To learn more about detector, start with the vignettes:
+#' \code{browseVignettes(package = "detector")}
+#'
+#' @docType package
+#' @name detector
+NULL
