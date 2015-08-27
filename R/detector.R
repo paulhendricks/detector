cce972a166a00dc4d6c67c05b59a62cd2e7467e3
paulhendricks <paul.hendricks.2013@owu.edu>
2015-08-27 16:55:44 +0000

    update to documentation removing vignette

--- R/detector.R
+++ R/detector.R
@@ -1,8 +1,5 @@
 #' detector: Detect Data Containing Personally Identifiable Information
 #'
-#' To learn more about detector, start with the vignettes:
-#' \code{browseVignettes(package = "detector")}
-#'
 #' @docType package
 #' @name detector
 NULL
