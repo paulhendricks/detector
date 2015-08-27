f21e4b8f6c52f1f13d44cb256fbab2a8b4d859ff
paulhendricks <paul.hendricks.2013@owu.edu>
2015-08-27 16:36:58 +0000

    added test

--- tests/testthat/test-national_identification_numbers.R
+++ tests/testthat/test-national_identification_numbers.R
@@ -3,10 +3,12 @@ library(detector)
 context("national identification numbers")
 
 test_that("Produces the correct output.", {
+  expect_equal(is_national_identification_number("hello"), TRUE)
   expect_equal(is_national_identification_number("000-00-0000"), TRUE)
 })
 
 test_that("Produces the correct output type.", {
+  expect_equal(is_national_identification_number("hello"), "logical")
   expect_is(is_national_identification_number("000-00-0000"), "logical")
 })
 
